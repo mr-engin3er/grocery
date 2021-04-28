ef06ba83830fb3be9302b5e95613871ed1938374
kumar-dheeraj <dheeraj.kumar@habilelabs.io>
2021-04-28 22:24:41 +0530

    feat(setup): project setup
    setup project in venv and customize settings module for diff env

---
+++ grocery/settings/production.py
@@ -0,0 +1,22 @@
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ["127.0.0.1"]
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = '/static/'
